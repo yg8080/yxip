d624bb5e958c2e50236099bb3cb6b931d9c8a047
yg8080 <chinascdz2050@gmail.com>
2025-05-27 16:59:22 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -4,9 +4,7 @@ import re
 import os
 
 # 目标URL列表
-urls = [ 
-        'https://ip.164746.xyz'
-        ]
+urls = ['https://ip.164746.xyz']
 
 # 正则表达式用于匹配IP地址
 ip_pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
